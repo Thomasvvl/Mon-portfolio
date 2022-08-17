19d7335e643538d43400f7b77a7978cfa24c3712
Thomasvvl <thomasvvl.974@gmail.com>
2022-08-17 12:28:22 +0400

    mise a jour

--- JS/index.js
+++ JS/index.js
@@ -30,17 +30,40 @@ function showSlides(n) {
   dots[slideIndex - 1].className += " active";
 }
 
-//MENU BURGER ---------------------------------------------------------------
+// //MENU BURGER ---------------------------------------------------------------
 
-const menuBtn = document.querySelector(".menu-btn");
-let menuOpen = false;
+// const menuBtn = document.querySelector(".menu-btn");
+// let menuOpen = false;
 
-menuBtn.addEventListener("click", () => {
-  if (!menuOpen) {
-    menuBtn.classList.add("open");
-    menuOpen = true;
-  } else {
-    menuBtn.classList.remove("open");
-    menuOpen = false;
-  }
+// menuBtn.addEventListener("click", () => {
+//   if (!menuOpen) {
+//     menuBtn.classList.add("open");
+//     menuOpen = true;
+//   } else {
+//     menuBtn.classList.remove("open");
+//     menuOpen = false;
+//   }
+// });
+
+const mainMenu = document.querySelector(".mainMenu");
+const closeMenu = document.querySelector(".closeMenu");
+const openMenu = document.querySelector(".openMenu");
+const menu_items = document.querySelectorAll("nav .mainMenu li a");
+
+openMenu.addEventListener("click", show);
+closeMenu.addEventListener("click", close);
+
+// close menu when you click on a menu item
+menu_items.forEach((item) => {
+  item.addEventListener("click", function () {
+    close();
+  });
 });
+
+function show() {
+  mainMenu.style.display = "flex";
+  mainMenu.style.top = "0";
+}
+function close() {
+  mainMenu.style.top = "-100%";
+}
